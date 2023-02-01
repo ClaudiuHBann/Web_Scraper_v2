1acee0e98dc3439fe2d0d91c2a4e6d95019eeded
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-02-01 02:39:45 +0200

    init

---
+++ Web Scraper/Main.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "Utility/Utility.h"
+
+int main() {
+    TRACE(L"penis");
+
+    return 0;
+}
---
+++ Web Scraper/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Windows.h>
+
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <string>
+
+using namespace std;
+using namespace chrono;
