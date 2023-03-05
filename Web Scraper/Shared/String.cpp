eed60e9e05bc022117041539daa97d0a03287d53
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-03-05 06:10:29 +0200

    surpressed tostringtype conversion possible loss of data and fixed a function

--- Web Scraper/Shared/String.cpp
+++ Web Scraper/Shared/String.cpp
@@ -2,46 +2,46 @@
 #include "String.h"
 
 namespace Shared {
-    namespace StringUtil {
-        String RemoveStringLead(const String& data, const String& lead) {
-            auto pos = data.find(lead);
-            return pos != String::npos ? data.substr(pos + lead.size()) : data;
-        }
+	namespace StringUtil {
+		String RemoveStringLead(const String& data, const String& lead) {
+			auto pos = data.find(lead);
+			return pos != String::npos ? data.substr(pos + lead.size()) : data;
+		}
 
-        String StringLead(const String& data, const String& lead) {
-            auto pos = data.find(lead);
-            return pos != String::npos ? data.substr(0, pos + lead.size()) : String();
-        }
+		String StringLead(const String& data, const String& lead) {
+			auto pos = data.find(lead);
+			return pos != String::npos ? data.substr(0, pos + lead.size()) : String();
+		}
 
-        String RemoveStringTrail(const String& data, const String& trail) {
-            auto pos = data.rfind(trail);
-            return pos != String::npos ? data.substr(pos + trail.size()) : data;
-        }
+		String RemoveStringTrail(const String& data, const String& trail) {
+			auto pos = data.rfind(trail);
+			return pos != String::npos ? data.substr(0, pos) : data;
+		}
 
-        String StringTrail(const String& data, const String& trail) {
-            auto pos = data.rfind(trail);
-            return pos != String::npos ? data.substr(0, pos + trail.size()) : String();
-        }
+		String StringTrail(const String& data, const String& trail) {
+			auto pos = data.rfind(trail);
+			return pos != String::npos ? data.substr(pos + trail.size()) : String();
+		}
 
-        bool Contains(const String& data, const String& query) {
-            return data.find(query) != String::npos;
-        }
+		bool Contains(const String& data, const String& query) {
+			return data.find(query) != String::npos;
+		}
 
-        /*vector<String> Split(const String& data, const String& delimiter) {
-            auto dataSplitted = data | views::split(delimiter);
-            return vector<String>(dataSplitted.begin(), dataSplitted.end());
-        }*/
+		/*vector<String> Split(const String& data, const String& delimiter) {
+			auto dataSplitted = data | views::split(delimiter);
+			return vector<String>(dataSplitted.begin(), dataSplitted.end());
+		}*/
 
-        vector<size_t> FindAllOccurrences(const String& data, const String& query) {
-            vector<size_t> occurrences;
+		vector<size_t> FindAllOccurrences(const String& data, const String& query) {
+			vector<size_t> occurrences;
 
-            auto pos = data.find(query);
-            while (pos != String::npos) {
-                occurrences.push_back(pos);
-                pos = data.find(query, pos + query.size());
-            }
+			auto pos = data.find(query);
+			while (pos != String::npos) {
+				occurrences.push_back(pos);
+				pos = data.find(query, pos + query.size());
+			}
 
-            return occurrences;
-        }
-    }
+			return occurrences;
+		}
+	}
 }
--- Web Scraper/Shared/String.h
+++ Web Scraper/Shared/String.h
@@ -1,11 +1,11 @@
 #pragma once
 
 namespace impl {
-    template<typename T>
-    inline constexpr bool is_basic_string_v = false;
+	template<typename T>
+	inline constexpr bool is_basic_string_v = false;
 
-    template<typename... T>
-    inline constexpr bool is_basic_string_v<basic_string<T...>> = true;
+	template<typename... T>
+	inline constexpr bool is_basic_string_v<basic_string<T...>> = true;
 }
 
 template<typename T>
@@ -13,52 +13,55 @@ inline constexpr bool is_basic_string_v = impl::is_basic_string_v<T>;
 
 namespace Shared {
 #if defined(_UNICODE) || defined(UNICODE)
-    using String = wstring;
-    using StringStream = wstringstream;
-    using IfStream = wifstream;
+	using String = wstring;
+	using StringStream = wstringstream;
+	using IfStream = wifstream;
 #else
-    using String = string;
-    using StringStream = stringstream;
-    using IfStream = ifstream;
+	using String = string;
+	using StringStream = stringstream;
+	using IfStream = ifstream;
 #endif // defined(_UNICODE) || defined(UNICODE)
 
-    template <typename T>
-    inline String ToString(const T& t) {
-        if constexpr (is_same_v<remove_const_t<remove_pointer_t<decay_t<T>>>, ::TCHAR> ||
-                      is_same_v<remove_const_t<remove_reference_t<decay_t<T>>>, String>) {
-            return t;
-        } else {
+	template <typename T>
+	inline String ToString(const T& t) {
+		if constexpr (is_same_v<remove_const_t<remove_pointer_t<decay_t<T>>>, ::TCHAR> ||
+			is_same_v<remove_const_t<remove_reference_t<decay_t<T>>>, String>) {
+			return t;
+		}
+		else {
 #if defined(_UNICODE) || defined(UNICODE)
-            return to_wstring(t);
+			return to_wstring(t);
 #else
-            return to_string(t);
+			return to_string(t);
 #endif // defined(_UNICODE) || defined(UNICODE)
-        }
-    }
+		}
+	}
 
-    template<typename StringType, typename StringFrom>
-    inline auto ToStringType(const StringFrom& strTo) {
-        static_assert(is_basic_string_v<StringFrom>, "The StringFrom is not a basic_string!");
-        static_assert(is_arithmetic_v<StringType>, "The StringType is not a built-in type!");
+	template<typename StringType, typename StringFrom>
+	inline auto ToStringType(const StringFrom& strTo) {
+		static_assert(is_basic_string_v<StringFrom>, "The StringFrom is not a basic_string!");
+		static_assert(is_arithmetic_v<StringType>, "The StringType is not a built-in type!");
 
-        if constexpr (is_same_v<typename StringFrom::value_type, StringType>) {
-            return strTo;
-        } else {
-            return basic_string<StringType, char_traits<StringType>, allocator<StringType>>(strTo.begin(), strTo.end());
-        }
-    }
+		if constexpr (is_same_v<typename StringFrom::value_type, StringType>) {
+			return strTo;
+		}
+		else {
+#pragma warning(suppress: 4244)
+			return basic_string<StringType, char_traits<StringType>, allocator<StringType>>(strTo.begin(), strTo.end());
+		}
+	}
 
-    namespace StringUtil {
-        String RemoveStringLead(const String& data, const String& lead);
-        String StringLead(const String& data, const String& lead);
+	namespace StringUtil {
+		String RemoveStringLead(const String& data, const String& lead);
+		String StringLead(const String& data, const String& lead);
 
-        String RemoveStringTrail(const String& data, const String& trail);
-        String StringTrail(const String& data, const String& trail);
+		String RemoveStringTrail(const String& data, const String& trail);
+		String StringTrail(const String& data, const String& trail);
 
-        bool Contains(const String& data, const String& query);
+		bool Contains(const String& data, const String& query);
 
-        vector<String> Split(const String& data, const String& delimiter);
+		vector<String> Split(const String& data, const String& delimiter);
 
-        vector<size_t> FindAllOccurrences(const String& data, const String& query);
-    }
+		vector<size_t> FindAllOccurrences(const String& data, const String& query);
+	}
 }
